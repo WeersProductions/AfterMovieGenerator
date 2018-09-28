70e9faa6c91d7b4c22614523110e56b83bdd382b
Floris <weersproductions@gmail.com>
2018-09-29 01:24:32 +0200

    Add basic UI buttons

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './app.css';
-import styled from 'react-emotion';
+import styled, { css } from 'react-emotion';
 import { Column, Row } from 'simple-flexbox';
+import DropZone from 'react-dropzone';
+import MusicPlayer from 'react-responsive-music-player';
+import ReactPlayer from 'react-player';
 
 const Input = styled('input')`
   padding: 0.5em;
@@ -24,13 +27,61 @@ const Button = styled('button')`
   border-radius: 3px;
 `;
 
+const Thumbnail = styled('img')`
+  width: 50%;
+  height: auto;
+  padding: 5px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+`;
+
+const Waveform = styled('img')`
+  width: 100%;
+  height: auto;
+  padding: 4px;
+  background-color: black;
+`;
+
+const DropZoneOwn = styled(DropZone)`
+  padding: 1rem;
+  border: 1px dashed #ddd;
+  border-radius: 5px;
+
+  &.active {
+    border-width: 2px;
+    border-color: #666;
+    border-style: dashed;
+    border-radius: 5px;
+  };
+
+  &.rejected {
+    borderStyle: 'solid';
+    borderColor: '#c66';
+    backgroundColor: '#eee;
+  };
+
+  &.disabled {
+    opacity: 0.5;
+  };
+
+  &.active {
+    borderStyle: 'solid';
+    borderColor: '#6c6';
+    backgroundColor: '#eee';
+  };
+`;
+
 export default class App extends Component {
   state = {
     waveform: null,
-    songId: ''
+    songId: '',
+    images: null,
+    music: null,
+    videoResult: null
   };
 
   componentDidMount() {
+    this.setState({ songId: '5b91297d6534697f83afaf3e' });
     // fetch('/api/getUsername')
     //   .then(res => res.json())
     //   .then(result => this.setState({ waveform: result.username }));
@@ -40,6 +91,10 @@ export default class App extends Component {
     this.setState({ songId: event.target.value });
   };
 
+  setVideoResult = (video) => {
+    this.setState({ videoResult: video });
+  };
+
   getWaveForm = () => {
     const { songId } = this.state;
     console.log(`get waveform for songId: ${songId}`);
@@ -47,29 +102,105 @@ export default class App extends Component {
     this.setState({ waveform: `/api/waveform/${songId}.png` });
   };
 
+  onDropMovieFiles = (acceptedFiles, rejectedFiles) => {
+    console.log(`Uploading ${acceptedFiles.length} video files`);
+    this.setState({
+      images: acceptedFiles
+    });
+  };
+
+  onDropAudioFiles = (acceptedFiles, rejectedFiles) => {
+    console.log(`Uploading ${acceptedFiles.length} audio files`);
+    console.log(acceptedFiles);
+    this.setState({
+      music: acceptedFiles
+    });
+  };
+
   render() {
-    // {username ? <h1>{`Hello ${username}`}</h1> : <h1>Hello.. please wait!</h1>}
-    const { waveform, songId } = this.state;
+    const {
+      waveform, songId, images, videoResult
+    } = this.state;
+    let { music } = this.state;
+    const imagesDrawn = [];
+    if (images) {
+      for (let i = 0; i < images.length; i++) {
+        console.log(images[i].preview);
+        imagesDrawn.push(
+          <div key={i}>
+            <Thumbnail src={images[i].preview} alt={images[i].name} />
+          </div>
+        );
+      }
+    }
+    if (music && !music.url) {
+      music = { url: music[0].preview, artist: ['unknown'], title: music[0].name };
+    }
+
     return (
       <div>
         <Column flexGrow={1}>
+          <Row>{videoResult ? <ReactPlayer url={videoResult.preview} playing /> : <div />}</Row>
+          <Row>
+            <Column flexGrow={1}>
+              <DropZoneOwn
+                onDrop={this.onDropAudioFiles}
+                accept="audio/*"
+                activeClassName="active"
+                acceptClassName="accept"
+                disabledClassName="disabled"
+                rejectClassName="reject"
+                multiple={false}
+                disableClick={music}
+              >
+                <Column flexGrow={1}>
+                  <Row horizontal="center">Drop audio</Row>
+                  {music ? <MusicPlayer playlist={[music]} /> : <div />}
+                </Column>
+              </DropZoneOwn>
+            </Column>
+
+            <Column flexGrow={1}>
+              <Button primary onClick={this.getWaveForm}>
+                Upload music
+              </Button>
+            </Column>
+          </Row>
           <Row horizontal="center">
-            {waveform ? <img src={waveform} alt="waveform" /> : <div />}
+            {waveform ? <Waveform src={waveform} alt="waveform" /> : <div />}
           </Row>
           <Row vertical="center">
-            <Column flexGrow={1} horiztonal="center">
-              <Input defaultValue="5b91297d6534697f83afaf3e" onChange={this.setSongId} />
+            <Column flexGrow={1}>
+              <Input defaultValue="songId" onChange={this.setSongId} />
             </Column>
-            <Column flexGrow={1} horiztonal="center">
+            <Column flexGrow={1}>
               <Button primary onClick={this.getWaveForm}>
                 Get waveform
               </Button>
             </Column>
           </Row>
           <Row>
-            <Button primary onClick={this.getWaveForm}>
-              Get movie
-            </Button>
+            <Column flexGrow={1}>
+              <DropZoneOwn
+                onDrop={this.onDropMovieFiles}
+                accept="image/*"
+                activeClassName="active"
+                acceptClassName="accept"
+                disabledClassName="disabled"
+                rejectClassName="reject"
+              >
+                <Column flexGrow={1}>
+                  <Row horizontal="center">Drop images</Row>
+                  {imagesDrawn}
+                </Column>
+              </DropZoneOwn>
+            </Column>
+
+            <Column flexGrow={1}>
+              <Button primary onClick={this.getWaveForm}>
+                Get movie
+              </Button>
+            </Column>
           </Row>
         </Column>
       </div>
